58d4d62f50b0b2f8db9658870e3b3063d1effc92
Kip <kpearce.dodon@gmail.com>
2023-03-24 08:16:47 -0400

    Introduced a Dead Zone

--- Motor.py
+++ Motor.py
@@ -1,8 +1,11 @@
-    class Motor>
+    class Motor:
         def __init__(self):
             self.speed=0.5
 
         def set_speed(self,speed):
+            speed=max(-1, speed) #says that if speed is 7 it makes it the highest number (max) which is 7
+            speed=min(1, speed) #says that if speed is 7 it makes it the lowest number (min) which is 1
+            speed=speed^3 #making a dead zone (you want the speed to maintain the same at the start then increase)
             self.speed = speed
 
         def speed_up(self):
